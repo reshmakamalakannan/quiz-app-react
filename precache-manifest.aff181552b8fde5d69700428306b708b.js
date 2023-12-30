7139aa0999ffd6b203e40b574527ca945d358573
reshma <reshma.kamal25@gmail.com>
2023-12-30 23:55:39 +0530

    Updates

---
+++ precache-manifest.aff181552b8fde5d69700428306b708b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e645cdcab6e1b820416",
+    "url": "/quiz-app-react/static/css/main.279827a7.chunk.css"
+  },
+  {
+    "revision": "4e645cdcab6e1b820416",
+    "url": "/quiz-app-react/static/js/main.4e645cdc.chunk.js"
+  },
+  {
+    "revision": "7b1328a8accb757dad00",
+    "url": "/quiz-app-react/static/js/1.7b1328a8.chunk.js"
+  },
+  {
+    "revision": "3ee223226084085a68bf",
+    "url": "/quiz-app-react/static/js/runtime~main.3ee22322.js"
+  },
+  {
+    "revision": "00c1b7d177ff2922ced3f88c77f4d8c7",
+    "url": "/quiz-app-react/static/media/res.00c1b7d1.jpg"
+  },
+  {
+    "revision": "2448c87844498f485dc04cb93f5beb23",
+    "url": "/quiz-app-react/static/media/quiz.2448c878.webp"
+  },
+  {
+    "revision": "1d2f362297cb6e771af87ba746569f5e",
+    "url": "/quiz-app-react/static/media/image1.1d2f3622.jpg"
+  },
+  {
+    "revision": "14fcd189633936157ee354b6a092169d",
+    "url": "/quiz-app-react/static/media/image2.14fcd189.png"
+  },
+  {
+    "revision": "15d37380549ff8eeda40cce8f6c8c836",
+    "url": "/quiz-app-react/static/media/image3.15d37380.png"
+  },
+  {
+    "revision": "9cfd87a3569c30feeeb21fc5f8588262",
+    "url": "/quiz-app-react/static/media/image4.9cfd87a3.jpg"
+  },
+  {
+    "revision": "39bd17fb7f8db913399c673b8a4d6907",
+    "url": "/quiz-app-react/static/media/thank.39bd17fb.GIF"
+  },
+  {
+    "revision": "a7e0e82ab4bdd385d990df68ee77f8e0",
+    "url": "/quiz-app-react/static/media/bg.a7e0e82a.webp"
+  },
+  {
+    "revision": "1691f7a954cc6e52acf22fe5d99eea14",
+    "url": "/quiz-app-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/quiz-app-react/precache-manifest.aff181552b8fde5d69700428306b708b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/quiz-app-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
